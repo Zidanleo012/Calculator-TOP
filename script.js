598d1f517683fc97719248d4ce8cae33d15c6d5a
Zidan leo permana <zidanleopermana012@gmail.com>
2024-08-21 23:43:05 +0700

    add code for operator

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let firstNum = 0;
-let secondNum = 0;
-let operator = 0;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -28,4 +25,53 @@ function operate(op, num1, num2) {
     }
 }
 
-console.log(operate('+', 4, 5))
+function reset() {
+    firstNum = 0;
+    secondNum = 0;
+    operator = '';
+    secondDisplay.textContent = '';
+}
+
+function checkNaN(number) {
+    if (isNaN(parseFloat(number))) {
+        secondDisplay.textContent = ''
+        return true;
+    }
+    return false;
+}
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = '';
+
+const operands = document.querySelectorAll('.operand>button');
+let display = document.querySelector('#display-text');
+let secondDisplay = document.querySelector('.second-display');
+let opDisplay = document.querySelector('#op-display');
+const opAndCal = document.querySelectorAll('.op-button>button');
+
+operands.forEach(item => {
+    item.addEventListener('click', (e) => {
+        if (display.textContent.length < 15) {
+            display.textContent += e.target.value;
+        }
+    })
+})
+
+opAndCal.forEach(item => {
+    item.addEventListener('click', e => {
+        if (!firstNum) {
+            firstNum = secondDisplay.textContent = parseFloat(display.textContent)
+            display.textContent = ''
+        } else if (e.target.id === 'operate'){
+            secondNum = parseFloat(display.textContent)
+            checkNaN(secondNum)
+            display.textContent = operate(operator, firstNum, secondNum)
+            console.log(display.textContent)
+            reset()
+        } 
+        operator = e.target.value;
+        opDisplay.textContent = e.target.value;
+        console.log(firstNum, operator, secondNum, checkNaN(firstNum))
+    })
+})
